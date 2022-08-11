4dd9e94701dcab9e40428b6a060ced5e4f22d967
anyelayoszo <anyelayoszo@gmail.com>
2022-08-11 23:37:32 +0300

    instructor added

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 5,
+    spaceBetween: 35,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+        1350: {
+            slidesPerView: 4,
+        },
+        1750: {
+            slidesPerView: 5,
+        },
+    },
+  });
+
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY >20){
